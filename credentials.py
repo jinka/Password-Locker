9b8f68ea3bd19449421d272c2b9f9849ed72180c
jinka <voiphandy@gmail.com>
2019-01-27 08:13:10 +0300

    Created user_list variable to store user objects

--- credentials.py
+++ credentials.py
@@ -2,14 +2,3 @@
     """
     Class that generates new instances of credentials
     """
-
-        def __init__(self,user_name,user_password,full_name,phone_number,email):
-
-      # docstring removed for simplicity
-
-        self.user_name = user_name
-        self.user_password = password
-        self.full_name = full_name
-        self.phone_number = number
-        self.email = email
-
--- user.py
+++ user.py
@@ -1,6 +1,15 @@
- class User:
+class User:
     """
     Class that generates new instances of users
     """
+    def __init__(self,user_name,user_password,full_name,phone_number,email):
 
-    pass
+       user_list = [] # Empty contact list
+
+      # docstring removed for simplicity
+
+        self.user_name = user_name
+        self.user_password = password
+        self.full_name = full_name
+        self.phone_number = number
+        self.email = email
