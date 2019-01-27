9c911ea2867fd2d19939419c557ecf1fe9393106
jinka <voiphandy@gmail.com>
2019-01-27 09:16:03 +0300

    Created user unit test file user_test.py

--- user.py
+++ user.py
@@ -2,14 +2,15 @@ class User:
     """
     Class that generates new instances of users
     """
-    def __init__(self,user_name,user_password,full_name,phone_number,email):
 
-       user_list = [] # Empty contact list
+    user_list = [] # Empty contact list
+
+    def __init__(self,user_name,user_password,full_name,phone_number,email):
 
       # docstring removed for simplicity
 
         self.user_name = user_name
-        self.user_password = password
+        self.user_password = user_password
         self.full_name = full_name
-        self.phone_number = number
+        self.phone_number = phone_number
         self.email = email
